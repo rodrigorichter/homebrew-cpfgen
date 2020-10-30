525168080840d24a4b7184f51f990033205402c1
rodrigorichter <guigorichter@gmail.com>
2020-10-30 09:16:11 -0300

    finalized formula

--- cpfgen.rb
+++ cpfgen.rb
@@ -1,31 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Cpfgen < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/rodrigorichter/cpfgen/archive/0.0.3.tar.gz"
-  sha256 "d270c220c4515eeb50eadec4595af903dfa014b99fbfaa7aa400512a2bce5b18"
-  license ""
-
-  # depends_on "cmake" => :build
+  desc "Prints a valid brazilian CPF number."
+  homepage "https://github.com/rodrigorichter/cpfgen"
+  url "https://github.com/rodrigorichter/cpfgen/archive/0.0.4.tar.gz"
+  sha256 "334eb4e4101514ad03b91fea4b637014dd2801030348dc1e4c1ea9473dc16dc3"
+  license "MIT"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
+    system "mkdir", "#{prefix}/bin"
     system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cpfgen`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    output = shell_output("#{bin}/cpfgen")
+    assert_equal(output.length, 12)
   end
 end
